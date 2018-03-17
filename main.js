b0c4d53d5f79f3eb0eafcc6c9574207e44113b79
jnieto11 <julionietoduran@gmail.com>
2018-03-16 20:04:19 -0500

    Ne commit

--- main.js
+++ main.js
@@ -18,12 +18,6 @@ const { autoUpdater } = require("electron-updater");
 autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = 'info';
 log.info('App starting...');
-autoUpdater.setFeedURL({
-    provider: 'github',
-    token: '1e9a6f5974362b8f4f97dd2af03d2d4b642b7312',
-    owner: '****',
-  });
-
 //-------------------------------------------------------------------
 // Define the menu
 //
