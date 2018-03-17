6c623fe8569e02e06778a155c6dfbf5c6a2c38c6
jnieto11 <julionietoduran@gmail.com>
2018-03-16 20:47:25 -0500

    commit t

--- main.js
+++ main.js
@@ -1,148 +1,108 @@
-// This is free and unencumbered software released into the public domain.
-// See LICENSE for details
-
-const { app, BrowserWindow, Menu, protocol, ipcMain } = require('electron');
+const {app, BrowserWindow, Notification,ipcMain, ipcRenderer} = require('electron')
 const log = require('electron-log');
+const {autoUpdater} = require("electron-updater");
+
 const path = require('path')
 const url = require('url')
-const { autoUpdater } = require("electron-updater");
+const AuthWindow = require('./auth');
 
-//-------------------------------------------------------------------
-// Logging
-//
-// THIS SECTION IS NOT REQUIRED
-//
-// This logging setup is not required for auto-updates to work,
-// but it sure makes debugging easier :)
-//-------------------------------------------------------------------
+//Logging
 autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = 'info';
 log.info('App starting...');
-//-------------------------------------------------------------------
-// Define the menu
-//
-// THIS SECTION IS NOT REQUIRED
-//-------------------------------------------------------------------
-let template = []
-if (process.platform === 'darwin') {
-    // OS X
-    const name = app.getName();
-    template.unshift({
-        label: name,
-        submenu: [
-            {
-                label: 'About ' + name,
-                role: 'about'
-            },
-            {
-                label: 'Quit',
-                accelerator: 'Command+Q',
-                click() { app.quit(); }
-            },
-        ]
-    })
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let win
+
+function createWindow () {
+  // Create the browser window.
+  win =  new BrowserWindow({width: 1120, height: 700, frame: false, minHeight: 610, minWidth: 850});
+
+  //win.webContents.openDevTools()
+
+  // and load the index.html of the app.
+  win.loadURL(url.format({
+    pathname: path.join(__dirname, '/app/index.html'),
+    protocol: 'file:',
+    slashes: true
+  }))
+
+
+  win.on('closed', () => {
+    win = null
+  })
 }
-
-
-//-------------------------------------------------------------------
-// Open a window that displays the version
-//
-// THIS SECTION IS NOT REQUIRED
-//
-// This isn't required for auto-updates to work, but it's easier
-// for the app to show a window than to have to click "About" to see
-// that updates are working.
-//-------------------------------------------------------------------
-let win;
 
 function sendStatusToWindow(text) {
-    log.info(text);
-    win.webContents.send('message', text);
+  log.info(text);
+  win.webContents.send('message', text);
 }
-function createDefaultWindow() {
-    win = new BrowserWindow({
-        icon: path.join(__dirname, 'build', 'icon.ico'),
-    })
 
-    win.maximize()
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'build', 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
-      win.webContents.openDevTools()
-    return win;
+global.openLoginServer = function(server_name, callback){
+  AuthWindow.startRequest(win, function(access_token, err) {
+    callback(err, access_token);
+  });
 }
+
 autoUpdater.on('checking-for-update', () => {
-    sendStatusToWindow('Checking for update...');
+  sendStatusToWindow('Buscando actualizaciones...');
 })
 autoUpdater.on('update-available', (info) => {
-    sendStatusToWindow('Update available.');
+  sendStatusToWindow('Actualización disponible');
 })
 autoUpdater.on('update-not-available', (info) => {
-    sendStatusToWindow('Update not available.');
+  sendStatusToWindow('Ya tienes la última versión.');
 })
 autoUpdater.on('error', (err) => {
-    sendStatusToWindow('Error in auto-updater. ' + err);
+  sendStatusToWindow('Error al actualizar.');
 })
 autoUpdater.on('download-progress', (progressObj) => {
-    let log_message = "Download speed: " + progressObj.bytesPerSecond;
-    log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
-    log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-    sendStatusToWindow(log_message);
+  let speed = ((progressObj.bytesPerSecond / 1000) / 1000).toFixed(1);
+  let transferred = ((progressObj.transferred / 1000) / 1000).toFixed(1);
+  let total = ((progressObj.total / 1000) / 1000).toFixed(1);
+  let percent = progressObj.percent.toFixed(1);
+
+  let log_message = "Download speed: " + speed + " Mb/s";
+  log_message = log_message + ' - Downloaded: ' + percent + '%';
+  log_message = log_message + ' (' + transferred + "/" + total + ')';
+  sendStatusToWindow(log_message);
 })
 autoUpdater.on('update-downloaded', (info) => {
-    sendStatusToWindow('Update downloaded');
+  sendStatusToWindow('Actualización descargada, se instalará en 5 segundos.');
 });
-app.on('ready', function () {
-    // Create the Menu
-    const menu = Menu.buildFromTemplate(template);
-    Menu.setApplicationMenu(menu);
 
-    createDefaultWindow();
-});
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    app.quit();
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (win === null) {
+    createWindow()
+  }
+})
+
+autoUpdater.on('update-downloaded', (info) => {
+  // Wait 5 seconds, then quit and install
+  // In your application, you don't need to wait 5 seconds.
+  // You could call autoUpdater.quitAndInstall(); immediately
+  setTimeout(function() {
+    autoUpdater.quitAndInstall();  
+  },5000)
+})
+
+app.on('ready', function()  {
+  autoUpdater.checkForUpdates();
 });
-
-//
-// CHOOSE one of the following options for Auto updates
-//
-
-//-------------------------------------------------------------------
-// Auto updates - Option 1 - Simplest version
-//
-// This will immediately download an update, then install when the
-// app quits.
-//-------------------------------------------------------------------
-app.on('ready', function () {
-    autoUpdater.checkForUpdatesAndNotify();
-});
-
-//-------------------------------------------------------------------
-// Auto updates - Option 2 - More control
-//
-// For details about these events, see the Wiki:
-// https://github.com/electron-userland/electron-builder/wiki/Auto-Update#events
-//
-// The app doesn't need to listen to any events except `update-downloaded`
-//
-// Uncomment any of the below events to listen for them.  Also,
-// look in the previous section to see them being used.
-//-------------------------------------------------------------------
-// app.on('ready', function()  {
-//   autoUpdater.checkForUpdates();
-// });
-// autoUpdater.on('checking-for-update', () => {
-// })
-// autoUpdater.on('update-available', (info) => {
-// })
-// autoUpdater.on('update-not-available', (info) => {
-// })
-// autoUpdater.on('error', (err) => {
-// })
-// autoUpdater.on('download-progress', (progressObj) => {
-// })
-// autoUpdater.on('update-downloaded', (info) => {
-//   autoUpdater.quitAndInstall();  
-// })
